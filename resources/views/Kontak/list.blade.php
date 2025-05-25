a1a408d2268c666409b616f978d7f59b7ab44093
kgdr26 <kangdru261199@gmail.com>
2025-05-26 05:54:54 +0700

    Update menu kontak

--- resources/views/Kontak/list.blade.php
+++ resources/views/Kontak/list.blade.php
@@ -1,4 +1,70 @@
 @extends('main')
 @section('content')
 
+    <!-- .page-content start -->
+    <div class="page-content custom-img-background dark page-title page-title-5 mb-100">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-12 start -->
+                <div class="col-md-12 centered">
+                    <div class="custom-heading style-1 triggerAnimation animated" data-animate='fadeInUp'>
+                        <h5 style="font-family:Trajan Pro Regular; color:#fbda9a;letter-spacing:2px;">Kontak</h5>
+                        <h3 style="font-family:baskervillenormal; text-transform:none; letter-spacing:-2px;">PT MANDALA MAKMUR SENOTSA</h3>
+                    </div><!-- .custom-heading.style-1 end -->
+                </div><!-- .col-md-12 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+      		 <!-- .page-content start -->
+        <div class="page-content">
+            <div class="container">
+                <!-- .row start -->
+                <div class="row mb-0">
+                    <!-- .col-md-12 start -->
+                    <div class="col-md-12 centered mb-0">
+                        <div class="custom-heading style-1">
+                             <h5 style="font-family:Trajan Pro Regular; color:#f37021;letter-spacing:2px;">Kunjungi Kami</h5>
+                            <h2>Lokasi Kami</h2>
+                        </div><!-- .custom-heading.style-1 end -->
+                    </div><!-- .col-md-12 end -->
+                </div><!-- .row end -->
+                <!-- .row start -->
+                <div class="row">
+                    <!-- .col-md-4 start -->
+                     <!-- .col-md-4 start -->
+                    <div class="col-md-4 col-sm-4 ">
+                        <div class="custom-heading style-2">
+                            <h4>Jakarta</h4>
+                            <div class="divider style-1">
+                                <i class="fa fa-circle hr-icon left"></i>
+                                <span class="hr-simple right"></span>
+                            </div>
+                        </div><!-- .custom-heading.style-2 end -->
+						
+                        <div style="font-family:MontserratBold;">ALAMAT:</div>
+                        <ul>
+                            <li>Head Office</li>
+                            <li>18 Office Park Lt.28 Suite A 2</li>
+                            <li>Jl. TB Simaputang No. 18, Kebagusan, Pasar Minggu,</li>
+                            <li>Jakarta Selatan, DKI Jakarta</li>
+                        </ul>
+                        <div style="font-family:MontserratBold;">KONTAK:</div>
+                        <ul>
+                            <li>(LINA) : +62 878-2080-2018</li>
+                            <li>(JO) : +62 898-0449-837</li>
+                        </ul>
+                        <div style="font-family:MontserratBold;">EMAIL:</div>
+                        <ul>
+                            <li>admin@mandalamakmursentosa.com</li>
+                            <li>sales@mandalamakmursentosa.com</li>
+                        </ul>
+
+
+                    </div><!-- .col-md-4 end -->
+                </div><!-- .row end -->
+            </div><!-- .container end -->
+        </div><!-- .page-content end -->
+
 @stop
